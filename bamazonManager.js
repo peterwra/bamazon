481677cb4078ffa8b7629387fb6c792ba965be7f
Peter W <42481836+peterwra@users.noreply.github.com>
2019-04-16 16:21:58 -0500

    More functionality added to bamazonManager.js file

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,21 +20,19 @@ var inquirer = require("inquirer");
 // Require table for displaying table data in a nice layout
 var formattable = require("table");
 
-// User choice for item id
-var intChoice = 0;
-
 // Function to display the information in the products table
 function displayProductTable() {
+    // Create array to hold data
+    var data = [];
+
+    // Header row
+    data.push(["--- Item Id ---", "--- Product Name ---", "--- Price ---", "--- Stock ---"]);
+
+    // Fill the data array from the database
     connection.query("SELECT item_id, product_name, price, stock_quantity FROM PRODUCTS", function (err, res) {
         if (err) {
             throw err;
         }
-
-        // Variable to store the data
-        var data = [];
-
-        // Create header row
-        data.push(["--- Item Id ---", "--- Product Name ---", "--- Price ---", "--- Stock ---"]);
 
         // Loop through the results and append to the data array
         for (var i = 0; i < res.length; i++) {
@@ -43,7 +41,39 @@ function displayProductTable() {
 
         // Output the data in a nicely formatted layout
         var output = formattable.table(data);
-        console.log(output);
+        console.log("\n"+output+"\n");
+        userPrompt();
+    })
+}
+
+function lowInventory() {
+    // Create array to hold data
+    var data = [];
+
+    // Fill the data array from the database
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM PRODUCTS WHERE stock_quantity<5", function (err, res) {
+        if (err) {
+            throw err;
+        }
+
+        // Check if we have low inventory
+        if (res.length > 0) {
+            // Header row
+            data.push(["--- Item Id ---", "--- Product Name ---", "--- Price ---", "--- Stock ---"]);
+
+            // Loop through the results and append to the data array
+            for (var i = 0; i < res.length; i++) {
+                data.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity]);
+            }
+
+            // Output the data in a nicely formatted layout
+            var output = formattable.table(data);
+            console.log("\n"+output+"\n");
+
+        } else {
+            console.log("\nNo low inventory!\n");
+        }
+
         userPrompt();
     })
 }
@@ -63,82 +93,115 @@ function userPrompt() {
             case "View Products for Sale":
                 displayProductTable();
                 break;
+            case "View Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
             case "Exit":
                 exitProgram();
                 break;
             default:
-                console.log("Invalid Selection");
+                console.log("\nInvalid Selection\n");
                 userPrompt();
         }
     })
 }
 
-// Prompt the user to enter a quantity
-function productQuantity(price, quantity) {
+// Add inventory to the store
+function addInventory() {
+    // Populate data array
+    var data = [];
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM PRODUCTS", function (err, res) {
+        if (err) {
+            throw err;
+        }
+
+        // Loop through the results and append to the data array
+        for (var i = 0; i < res.length; i++) {
+            data.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity]);
+        }
+    })
+
     inquirer.prompt({
         type: "input",
         name: "userChoice",
-        message: "Please enter the quantity you would like to purchase:"
+        message: "Please enter the item number you would like to add inventory to:"
     }).then(function (user) {
-        var userQuantity = parseInt(user.userChoice);
+        var itemId = parseInt(user.userChoice);
 
-        // If user entered something other than a number, call the function again.
-        if (isNaN(userQuantity)) {
-            console.log("You need to enter a number.");
-            productQuantity(price, quantity);
+        // If user didn't enter a number, display alert and call the function again.
+        if (isNaN(itemId)) {
+            console.log("\nYou need to enter a number.\n");
+            addInventory();
         } else {
-            // Verify the user entered a valid quantity greater than 0
-            if (userQuantity <= 0) {
-                console.log("Please enter a quantity greater than 0.");
-                productQuantity();
-            } else {
-                // Cannot buy if there is not enough stock on hand.
-                if (userQuantity > quantity) {
-                    console.log("You want to buy " + userQuantity + " but we only have " + quantity + " in stock. Sorry!");
-                    productQuantity(price, quantity);
-                } else {
-                    // Valid quantity requested, purchase the product.
-                    purchaseProduct(price, quantity, userQuantity);
+            // If the item number doesn't exist, display alert.
+            var isFound = false;
+            for (var i = 1; i < data.length; i++) {
+                if (itemId == data[i][0]) {
+                    isFound = true;
+                    stockQuantity = parseInt(data[i][3]);
+                    break;
                 }
+            }
+
+            // Item not found, have the manager enter the item id again
+            if (!isFound) {
+                console.log("\nThe item id you entered does not exist. Please enter an existing item id.\n");
+                addInventory();
+            } else {
+
+                // User entered a valid item, proceed to quantity.
+                updateStock(itemId, stockQuantity);
             }
         }
     })
 }
 
-// Complete the purchase order
-function purchaseProduct(itemPrice, itemQuantity, purchaseQuantity) {
-    console.log("Update item id " + intChoice + " price " + itemPrice + " quantity " + itemQuantity);
-    console.log("Completing your purchase...");
-    var updateQuery = connection.query("UPDATE PRODUCTS SET ? WHERE ?",
-        [
-            { stock_quantity: itemQuantity - purchaseQuantity },
-            { item_id: intChoice }
-        ],
-        function (error, result) {
-            if (error) {
-                throw error;
+// Prompt the user to enter a quantity
+function updateStock(itemId, stockQuantity) {
+    inquirer.prompt({
+        type: "input",
+        name: "userChoice",
+        message: "Please enter the quantity you would add to the inventory:"
+    }).then(function (user) {
+        var userQuantity = parseInt(user.userChoice);
+
+        // If user entered something other than a number, call the function again.
+        if (isNaN(userQuantity)) {
+            console.log("\nYou need to enter a number.\n");
+            updateStock(itemId, stockQuantity);
+        } else {
+            // Verify the user entered a valid quantity greater than 0
+            if (userQuantity <= 0) {
+                console.log("\nPlease enter a quantity greater than 0.\n");
+                updateStock(itemId, stockQuantity);
             } else {
-                showPurchaseResults(itemPrice, purchaseQuantity);
+                // Valid quantity entered, increase stock.
+                var updateQuery = connection.query("UPDATE PRODUCTS SET ? WHERE ?",
+                    [
+                        { stock_quantity: stockQuantity + userQuantity },
+                        { item_id: itemId }
+                    ],
+                    function (error, result) {
+                        if (error) {
+                            throw error;
+                        } else {
+                            console.log("\nSuccessfully updated stock!\n");
+                            userPrompt();
+                        }
+                    });
             }
-        });
-}
-
-// Show the totals to the user
-function showPurchaseResults(price, quantity) {
-    // Total cost for the purchase
-    var totalCost = (price * quantity).toFixed(2);
-    console.log("You credit card has been charged $" + totalCost);
-    console.log("Thank you for using Bamazon, where the prices are always lower than Amazon!\nHope to see you again soon!");
-
-    // Exit the program
-    exitProgram();
+        }
+    })
 }
 
 // Exit the program
 function exitProgram() {
-    console.log("Thank you! Exiting now...")
+    console.log("\nThank you for using the manager application! Exiting now...\n")
     connection.end();
 }
 
-// Display product information to the user
+// Display user prompt
 userPrompt();
